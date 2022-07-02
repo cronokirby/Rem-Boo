93e13e011eaf74ba804f935f07fce6564f4ce5fd
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-07-02 12:33:42 +0200

    Define MultiQueue struct

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,3 +1,5 @@
+use std::ops::Mul;
+
 /// A MultiBuffer holds a single buffer for each data size.
 ///
 /// The reason to do this is for efficiency, so that we can always have
@@ -34,5 +36,29 @@ impl MultiBuffer {
     /// Iterate over all the u64s in the buffer.
     pub fn iter_u64(&self) -> impl Iterator<Item = &u64> {
         self.u64s.iter()
+    }
+}
+
+/// A wrapper over a MultiBuffer providing a FIFO queue.
+///
+/// This is useful in situations where we need to read the elements of a buffer
+/// in sequence.
+pub struct MultiQueue {
+    buffer: MultiBuffer,
+    /// The next index to read a u64 from the buffer.
+    i_u64: usize,
+}
+
+impl MultiQueue {
+    /// Create a new queue by wrapping a buffer.
+    pub fn new(buffer: MultiBuffer) -> Self {
+        Self { buffer, i_u64: 0 }
+    }
+
+    /// Read the next u64 value from the queue.
+    pub fn next_u64(&mut self) -> u64 {
+        let out = self.buffer.u64s[self.i_u64];
+        self.i_u64 += 1;
+        out
     }
 }
