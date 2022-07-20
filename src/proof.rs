e18151042fec577f92a9fa9d76cfaf59a2097ac4
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-07-20 20:55:47 +0200

    Add test for xor

--- src/proof.rs
+++ src/proof.rs
@@ -1007,4 +1007,24 @@ mod test {
         private.push_u64(0xDEADBEEF);
         assert_instance(b"context", &program, &public, &private)
     }
+
+    #[test]
+    fn test_xor() {
+        let program = Program {
+            public_size: 1,
+            private_size: 2,
+            instructions: vec![
+                PushPrivate(0),
+                PushPrivate(1),
+                Binary(Xor, Top),
+                AssertEq(Public(0)),
+            ],
+        };
+        let mut public = MultiBuffer::new();
+        public.push_u64(0b11011);
+        let mut private = MultiBuffer::new();
+        private.push_u64(0b00111);
+        private.push_u64(0b11100);
+        assert_instance(b"context", &program, &public, &private)
+    }
 }
