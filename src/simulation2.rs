81eeeac572cf05da2947b0ca6a90e5a360a1e2d5
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-08-07 19:28:31 +0200

    Define creation for Tracer type

--- src/simulation2.rs
+++ src/simulation2.rs
@@ -1,5 +1,24 @@
-use crate::bits::{Bit, BitBuf};
+use crate::{
+    bits::{Bit, BitBuf},
+    circuit::Circuit,
+};
 
 struct Tracer {
+    mem: BitBuf,
     trace: BitBuf,
+}
+
+impl Tracer {
+    /// Create a new tracer, with a given input buffer.
+    ///
+    /// We also take the circuit as input, since we use the metadata on input
+    /// lengths to setup various internal data structures.
+    pub fn new(input: &BitBuf, circuit: &Circuit) -> Self {
+        assert!(input.len() == circuit.priv_size);
+        // Setup memory to contain the input and then zero bits.
+        let mut mem = input.clone();
+        mem.resize(circuit.mem_size);
+        let trace = BitBuf::with_capacity(circuit.trace_size);
+        Self { mem, trace }
+    }
 }
