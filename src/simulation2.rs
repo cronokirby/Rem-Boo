2e38b4043f3a6ec85d15c93ec11cca6a202feea5
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-08-20 20:37:43 +0200

    Add initialization for Simulator

--- src/simulation2.rs
+++ src/simulation2.rs
@@ -43,8 +43,7 @@ impl<'a> Tracer<'a> {
             Instruction::Xor(a, b) => self.mem.push(self.mem.read(a) ^ self.mem.read(b)),
             Instruction::And(a, b) => {
                 let c = self.mem.read(a) & self.mem.read(b);
-                
-            },
+            }
         }
     }
 
@@ -68,13 +67,61 @@ pub struct PartyMasks {
     pub and_val_masks: BitBuf,
 }
 
-struct Simulator {}
+struct Party<'a> {
+    mem: BitBuf,
+    out: BitBuf,
+    and_val_masks: BitQueue<'a>,
+    and_out_masks: BitQueue<'a>,
+}
 
-pub fn simulate(
-    circuit: &Circuit,
-    public_input: &BitBuf,
-    masked_input: &BitBuf,
-    masks: &[PartyMasks],
-) -> Vec<BitBuf> {
-    todo!()
+impl<'a> Party<'a> {
+    fn new(circuit: &Circuit, masks: &'a PartyMasks) -> Self {
+        let mut mem = masks.input_masks.clone();
+        mem.increase_capacity_to(circuit.mem_size);
+        let out = BitBuf::new();
+        let and_val_masks = BitQueue::new(&masks.and_val_masks);
+        let and_out_masks = BitQueue::new(&masks.and_out_masks);
+        Self {
+            mem,
+            out,
+            and_val_masks,
+            and_out_masks,
+        }
+    }
+}
+
+struct Simulator<'a> {
+    circuit: &'a Circuit,
+    input_mem: BitBuf,
+    parties: Vec<Party<'a>>,
+}
+
+impl<'a> Simulator<'a> {
+    fn new(circuit: &'a Circuit, masked_input: &BitBuf, masks: &'a [PartyMasks]) -> Self {
+        let mut input_mem = masked_input.clone();
+        input_mem.increase_capacity_to(circuit.mem_size);
+        let parties = masks
+            .iter()
+            .map(|masks| Party::new(circuit, masks))
+            .collect();
+        Self {
+            circuit,
+            input_mem,
+            parties,
+        }
+    }
+
+    fn run(&mut self) {
+        todo!()
+    }
+
+    fn output(self) -> Vec<BitBuf> {
+        self.parties.into_iter().map(|party| party.out).collect()
+    }
+}
+
+pub fn simulate(circuit: &Circuit, masked_input: &BitBuf, masks: &[PartyMasks]) -> Vec<BitBuf> {
+    let mut simulator = Simulator::new(circuit, masked_input, masks);
+    simulator.run();
+    simulator.output()
 }
