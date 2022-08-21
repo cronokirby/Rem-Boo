9d56e69e265a223366e7ee2f43c191c5c46075e7
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-08-21 23:36:55 +0200

    Remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,9 @@
 mod baker;
 mod bits;
 pub mod circuit;
-mod buffer;
-pub mod bytecode;
-mod constants;
 mod interpreter;
-mod number;
 mod proof;
-mod rng;
 mod simulation;
-mod simulation2;
 
 pub use proof::{prove, verify, Proof};
-pub use circuit::{Circuit, Instruction};
-pub use simulation2::Error;
+pub use simulation::Error;
