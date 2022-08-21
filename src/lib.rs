a1d3267835b299a906efc31645cde0829febcbbc
Lúcás Críostóir Meier <lucas@cronokirby.com>
2022-08-21 23:40:47 +0200

    Move error to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 mod baker;
 mod bits;
 pub mod circuit;
+mod error;
 mod interpreter;
 mod proof;
 mod simulation;
 
 pub use proof::{prove, verify, Proof};
-pub use simulation::Error;
+pub use error::Error;
